9596b6c990be47a4af681653068e46d822dc6af0
Taylor Fang <stu005606@gmail.com>
2017-08-07 17:39:58 +0800

    Completed the example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,60 @@
 const three = require('three');
-const initialScene = require('../assets/initialScene.json');
+const Swiper = require('swiper');
+import { SceneManager } from './SceneManager';
+
+// Here we create a new instance of Swiper
+let controller = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: '.swiper-pagination',
+    keyboardControl: true,
+    simulateTouch: true,
+    threshold: 50,
+    hashnav: true,
+    speed: 700,
+    scrollbarHide: true,
+    followFinger: false,
+    touchAngle: 45,
+    effect: "fade",
+    fade: {
+        crossFade: true
+    },
+    onTap: function () {
+        controller.slideNext();
+    }
+});
 
 let loader = new three.ObjectLoader();
 
-loader.load(initialScene, loadedScene => {
-    // Code to manipulate loadedScene here
-    
+loader.load('../assets/initialScene2.json', loadedScene => {
+
+    let sceneManager = new SceneManager(loadedScene);
+    sceneManager.firstScene();
+
+    // This will make swiper call delegate update with the proper index when the user triggers a slide change
+    controller.on('onSlideChangeStart', function () {
+        delegateUpdate(controller.activeIndex)
+    });
+
+    // This function will call the proper scene on SceneManager depending on the index passed in
+    function delegateUpdate(index) {
+        // If the user triggers a scene change before the previous scene's animations finished, we'll want to stop
+        // and clear those animations
+        sceneManager.clearCurrentAnimation();
+        
+        // Here we use a case statement to trigger the proper scene based on the index
+        switch (index) {
+            case 0:
+                sceneManager.firstScene();
+                break;
+            case 1:
+                sceneManager.secondScene();
+                break;
+            case 2:
+                sceneManager.thirdScene();
+        }
+
+        // Finally we start the scene's animation sequence
+        sceneManager.startCurrentAnimation();
+    }
+
 })
